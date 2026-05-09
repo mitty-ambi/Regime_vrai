c7f618c69be0ca8fe71188155e4dbaaf35c2bf79
mahatoky <andrian.mahatoky@gmail.com>
2026-05-09 14:57:11 +0300

    ajout de fonction pour recuperer l objectif d un utilisateur specifique

--- app/Models/Objectif.php
+++ app/Models/Objectif.php
@@ -8,5 +8,14 @@ class Objectif extends Model
     protected $primaryKey = 'id';
     protected $allowedFields = ['nom'];
     protected $useTimestamps = false;
+
+    //recuperer l objectif de l utilisateur
+    public function getObjectif($idUtilisateur) {
+        
+        return $this->select("objectifs.*")
+                ->join("utilisateur_objectifs","objectifs.id = utilisateur_objectifs.objectif_id")
+                ->where("utilisateur_objectifs.utilisateur_id",$idUtilisateur)
+                ->first();
+    }
 }
 ?>
