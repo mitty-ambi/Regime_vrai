2b64a203f1b6a52cc0b67e8fcf41faf921368496
mahatoky <andrian.mahatoky@gmail.com>
2026-05-09 15:25:12 +0300

    implementation fonction pour haugmenter ou diminuer le poid avec une sueil

--- app/Models/Regime.php
+++ app/Models/Regime.php
@@ -8,16 +8,32 @@ class Regime extends Model
     protected $primaryKey = 'id';
     protected $allowedFields = ['nom', 'type', 'prix', 'duree', 'variation_poids', 'pourcentage_viande', 'pourcentage_poisson', 'pourcentage_volaille'];
     protected $useTimestamps = false;
+    
+    //reucuper les regime pour haugmenter le poid jusqu a une certain seuil
+    public function getRegimeDiminuateurPoidAvecSeuil($seuil) {
+        return $this->select("*")
+        ->where("variation_poids <",0)
+        ->where("variation_poids >=",$seuil)
+        ->findAll();
+    }
+
+    //reucuper les regime pour haugmenter le poid jusqu a une certain seuil
+    public function getRegimeAugmentateurPoidAvecSeuil($seuil) {
+        return $this->select("*")
+        ->where("variation_poids >",0)
+        ->where("variation_poids <=",$seuil)
+        ->findAll();
+    }
 
     //recuperer les regime pour dimuner le poid
-    public function getRegimeDiminuePoid() {
+    public function getRegimeDiminuateurPoid() {
         return $this->select("*")
         ->where("variation_poids <",0)
         ->findAll();
     }
 
     //recuperer les regime pour haugmenter le poid
-    public function getRegimeAugmenterPoid() {
+    public function getRegimeAugmenteurPoid() {
         return $this->select("*")
         ->where("variation_poids >",0)
         ->findAll();
