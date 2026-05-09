475e25b10c07b859672ee4fb959f309e1464ce6c
mahatoky <andrian.mahatoky@gmail.com>
2026-05-09 21:17:56 +0300

    controle de l affichage des regime quand l user a pour objectif Imc ideal

--- app/Controllers/RegimeController.php
+++ app/Controllers/RegimeController.php
@@ -8,16 +8,19 @@ ini_set('display_errors', 1);
 use App\Controllers\BaseController;
 use App\Models\Regime;
 use App\Models\Objectif;
+use App\Models\SanteModel;
 
 class RegimeController extends BaseController
 {
     protected $regimeModel;
     protected $objectifModel;
+    protected $santeModel;
 
     public function __construct()
     {
         $this->regimeModel = new Regime();
         $this->objectifModel = new Objectif();
+        $this->santeModel = new SanteModel();
     }
 
     public function go_to_suggest()
@@ -33,12 +36,29 @@ class RegimeController extends BaseController
         $objectif_id = $this->request->getGet("objectif_id");
         $durrer = $this->request->getGet("durrer");
         $preference = $this->request->getGet("preference");
+        $user = session()->get("utilisateur");
+
 
         //recuperer l objectif de poid
         $variationVoulu = $this->request->getGet("variationVoulu");
 
         //verfier quelle est l objecif selectionner
         $objectif = $this->objectifModel->find($objectif_id);
+
+        if ($objectif['nom'] === 'IMC ideal') {
+
+            $dataImcIdeal = $this->santeModel->getInfoForImcIdeal($user['id']);
+            $variationVoulu = $dataImcIdeal['variation_poid'];
+
+            if ($variationVoulu < 0) {
+                $data['liste_regime']  = $this->regimeModel->getSuggestionDiminuateurPoid($durrer, $preference, $variationVoulu);
+            }
+            if ($variationVoulu > 0) {
+                $data['liste_regime']  = $this->regimeModel->getSuggestionHaugmenterPoid($durrer, $preference, $variationVoulu);
+            }
+            $data['data_imc_ideal'] = $dataImcIdeal;
+        }
+
         if ($objectif['nom'] === 'augmentation') {
             $data['liste_regime'] = $this->regimeModel->getSuggestionHaugmenterPoid($durrer, $preference, $variationVoulu);
         }
@@ -46,8 +66,8 @@ class RegimeController extends BaseController
         if ($objectif['nom'] === 'reduction') {
             $data['liste_regime'] = $this->regimeModel->getSuggestionDiminuateurPoid($durrer, $preference, (-1) * $variationVoulu);
         }
-        $data['liste_objectif'] = $this->objectifModel->findAll();
 
+        $data['liste_objectif'] = $this->objectifModel->findAll();
         return view('regime/SuggestRegime', $data);
     }
 
