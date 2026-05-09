fbeb1967757b3f54968d366aa40389677ade64ad
mahatoky <andrian.mahatoky@gmail.com>
2026-05-09 17:59:24 +0300

    rearangement des fonction pour une meilleur lisibilite

--- app/Models/Regime.php
+++ app/Models/Regime.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Models;
+
 use CodeIgniter\Model;
 use WeakReference;
 
@@ -9,68 +11,80 @@ class Regime extends Model
     protected $primaryKey = 'id';
     protected $allowedFields = ['nom', 'type', 'prix', 'duree', 'variation_poids', 'pourcentage_viande', 'pourcentage_poisson', 'pourcentage_volaille'];
     protected $useTimestamps = false;
-    
+
+    //suggestons des regimes augmentateur de poid
+    public function getSuggestionHaugmenterPoid($durer, $preference, $seuilVariationPoidMax) {
+        
+    }
+    //suggestion des regimes diminuateur de poid
+    public function getSuggestionDiminuateurPoid($durer, $preference, $seuilVariationPoidMin)
+    {
+        return $this->whereDiminuateurPoid()
+            ->whereDurrerInferieur($durer)
+            ->preference($preference)
+            ->whereSueiVariationPoidlMin($seuilVariationPoidMin)
+            ->findAll();
+    }
+
+
     //recuperer les regimes selons une durer determiner
-    public function whereDurrerInferieur($durrer) {
-        return $this->where("duree <= ", $durrer)->findAll();
+    public function whereDurrerInferieur($durrer)
+    {
+        return $this->where("duree <= ", $durrer)->orderBy('duree', 'DESC');
     }
 
     //recuperer les reigime avec preference en 
-    public function wherePreferenceVolaille() {
-        return $this->wherePreference($this,"volaille")->orderBy('pourcentage_volaille',"DESC")->findAll();
+    public function wherePreferenceVolaille()
+    {
+        return $this->wherePreference($this, "volaille")->orderBy('pourcentage_volaille', "DESC")->findAll();
     }
 
     //recuperer les reigime avec preference en viande
-    public function wherePreferenceViande() {
-        return $this->wherePreference($this,"viande")->orderBy('pourcentage_viande',"DESC")->findAll();
+    public function wherePreferenceViande()
+    {
+        return $this->wherePreference($this, "viande")->orderBy('pourcentage_viande', "DESC")->findAll();
     }
 
 
     //recuperer les reigime avec preference en poisson
-    public function wherePreferencePoisson() {
-        return $this->wherePreference($this,"poisson")->orderBy('pourcentage_poisson',"DESC")->findAll();
+    public function preferencePoisson()
+    {
+        return $this->orderBy('pourcentage_poisson', "DESC")->findAll();
     }
 
     //recuperer les regimes  avec une preference en viande , poisson , ou volaille
-    public function wherePreference($query,$elementPreferer) {
-        if($elementPreferer == "poisson") {
-            $query->where("pourcentage_viande <= pourcentage_poisson")
-            ->where("pourcentage_volaille <= pourcentage_poisson");
+    public function preference($elementPreferer)
+    {
+        if ($elementPreferer == "poisson") {
+            $this->orderBy('pourcentage_poisson','DESC');
         } else if ($elementPreferer == "viande") {
-            $query->where("pourcentage_poisson <= pourcentage_viande")
-            ->where("pourcentage_volaille <= pourcentage_viande");
+            $this->orderBy('pourcentage_viande','DESC');
         } else if ($elementPreferer == "volaille") {
-            $query->where("pourcentage_poisson <= pourcentage_volaille")
-            ->where("pourcentage_viande <= pourcentage_volaille");
+            $this->orderBy('pourcentage_volaille','DESC');
         }
-        return $query;
+        return $this;
     }
 
     //reucuper les regime pour haugmenter le poid jusqu a une certain seuil
-    public function getRegimeDiminuateurPoidAvecSeuil($seuil) {
-        return $this->where("variation_poids <",0)
-        ->where("variation_poids >=",$seuil)
-        ->findAll();
+    public function whereSueiVariationPoidlMin($seuil)
+    {
+        return  $this->where("variation_poids >=", $seuil);
     }
 
-    //reucuper les regime pour haugmenter le poid jusqu a une certain seuil
-    public function getRegimeAugmentateurPoidAvecSeuil($seuil) {
-        return $this->select("variation_poids >",0)
-        ->where("variation_poids <=",$seuil)
-        ->findAll();
+    public function whereSueiVariationPoidlMax($seuil)
+    {
+        return $this->where("variation_poids <=", $seuil);
     }
 
     //recuperer les regime pour dimuner le poid
-    public function getRegimeDiminuateurPoid() {
-        return $this->where("variation_poids <",0)
-        ->findAll();
+    public function whereDiminuateurPoid()
+    {
+        return $this->where("variation_poids <", 0);
     }
 
     //recuperer les regime pour haugmenter le poid
-    public function getRegimeAugmenteurPoid() {
-        return $this->select("*")
-        ->where("variation_poids >",0)
-        ->findAll();
+    public function whereAugmenteurPoid()
+    {
+        return $this->where("variation_poids >", 0);
     }
 }
-?>
