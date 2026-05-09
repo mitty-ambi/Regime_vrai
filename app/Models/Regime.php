87f70560e08d0dbe75f8d1a67b0a78b3c4149f2e
mahatoky <andrian.mahatoky@gmail.com>
2026-05-09 15:40:39 +0300

    fonction pour recuperer les regime avec une preference en poisson

--- app/Models/Regime.php
+++ app/Models/Regime.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 use CodeIgniter\Model;
+use WeakReference;
 
 class Regime extends Model
 {
@@ -9,6 +10,21 @@ class Regime extends Model
     protected $allowedFields = ['nom', 'type', 'prix', 'duree', 'variation_poids', 'pourcentage_viande', 'pourcentage_poisson', 'pourcentage_volaille'];
     protected $useTimestamps = false;
     
+
+    //recuperer les reigime avec preference en poisson
+    public function getRegimePreferencePoisson() {
+        return $this->wherePreference($this,"poisson")->findAll();
+    }
+
+    //recuperer les regimes  avec une preference en viande , poisson , ou volaille
+    public function wherePreference($query,$elementPreferer) {
+        if($elementPreferer == "poisson") {
+            $query->where("pourcentage_viande <= pourcentage_poisson")
+            ->where("pourcentage_volaille <= pourcentage_poisson");
+        }
+        return $query;
+    }
+
     //reucuper les regime pour haugmenter le poid jusqu a une certain seuil
     public function getRegimeDiminuateurPoidAvecSeuil($seuil) {
         return $this->select("*")
