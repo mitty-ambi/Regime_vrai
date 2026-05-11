9cb6f1d8e0f001aa9e5cf402f59eab3eaa9ca3d4
ambintsou_log <ambintsoandria2@gmail.com>
2026-05-11 08:10:10 +0300

    a
    a

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <div class="logo">
-            🥗 Régime app update mety ve ??
+            🥗 Régime app
         </div>
         <div class="nav-right">
             <?php if (session()->get('is_logged_in')): ?>
