5e54bce26e94d0efc8df2836250e6160ddada7e3
mahatoky <andrian.mahatoky@gmail.com>
2026-05-11 08:09:53 +0300

    ajustement du route pour afiare foncitonner le susggetion

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,7 @@ $routes->get('/Regime/update/(:num)', 'RegimeController::update/$1');
 $routes->post('/Regime/edit/(:num)', 'RegimeController::edit/$1');
 $routes->get('/Regime/supprimer/(:num)', 'RegimeController::supprimer/$1');
 // Regime - suggestion de regime
-$routes->get('/Regime/suggest', 'RegimeController::go_to_suggest');
+$routes->get('/Regime/suggest', 'RegimeController::suggest');
 
 // Activites - CRUD complet
 $routes->get('/Activites/add', 'ActiviteController::go_to_activite');
